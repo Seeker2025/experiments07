1476c6e3e89544e3773584ef382d70be9e215b1f
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-03-12 16:29:47 +0200

    go on

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+console.log('a');
+console.log('b');
+
+const ourPromise = new Promise((resolve, reject) => {
+    let number = Math.random()>.5;
+    if (number) {
+        resolve('Success!');
+    } else {
+        reject(console.log('Failure!'));
+    }
+}).then(data => console.log('Данні з виконаного проміса: ', data))
+    .catch(error => console.log('Помилка з виконаного проміса: ', error))
+    .finally(() => console.log('Проміс завершено!'));
