fefab978af17f6962af261172ce56cb00d68096b
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-03-19 13:29:24 +0200

    go on

--- src/js/page_3.js
+++ src/js/page_3.js
@@ -1,12 +1,15 @@
-// console.log('q');
 const NOTIFICATION_DELAY = 3000;
 let timeoutId = null;
 
 const refs = {
     notification: document.querySelector('.js-alert'),
-    press: document.querySelector('.press'),
+    press: document.querySelector('.box_press'),
+    text: document.querySelector('.box_will'),
 };
 
+// refs.text.classList.add('hide');
+// refs.press.classList.add('hide');
+//  refs.text.classList.add('hide');
 // hideNotification();
 showNotification();
  console.log(timeoutId);
@@ -14,28 +17,33 @@ showNotification();
 refs.notification.addEventListener('click', onNotificationClick);
 
 console.log(refs.notification);
+// refs.press.classList.add('hide');
 
 function onNotificationClick() {
   console.log('click');
-  hideNotification();
+    hideNotification();
+    refs.press.classList.remove('hide');
+    refs.text.classList.add('hide');
     clearTimeout(timeoutId);
     // refs.press.classList.remove('press-js');
 }
 
 function showNotification() {
     refs.notification.classList.add('is-visible');
-
+    refs.press.classList.add('hide');
+    refs.text.classList.remove('hide');
+   
     timeoutId = setTimeout(() => {
         console.log(`We close the alert automatically so that it doesn't hang`);
         hideNotification();
-        
-        
+        refs.press.classList.remove('hide');
+        refs.text.classList.add('hide');
     }, NOTIFICATION_DELAY);
-   
+    
 }
 
 function hideNotification() {
-  refs.notification.classList.remove('is-visible');
+    refs.notification.classList.remove('is-visible');
 }
 
 document.addEventListener('keydown', (evt) => {
@@ -43,10 +51,5 @@ document.addEventListener('keydown', (evt) => {
         console.log('KeyH');
         showNotification();
     }       
-  
 });
 
-function pressAdd() {
-    refs.press.classList.add('press-js');
-}
-// pressAdd()
